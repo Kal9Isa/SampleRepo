e2a7a7c081f9a34809212a96f79950f3ea917d42
Ali Salehi <docalisa_it@yahoo.com>
2020-03-23 00:26:38 +0430

    printf and formatting tip

---
+++ 2-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	/* %d -->integer 
+		%s --> strings
+		%f --> floating point
+		%c --> single character
+		%% --> percent sign*/
+	printf("This value is: %d\n", 27);
+	printf("You're a %c %s!\n", 'c', "programmer");
+	return(0);
+}
